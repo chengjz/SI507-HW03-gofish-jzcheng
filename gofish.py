8f027a4e8c86ebcf1f12d71b6347f09fc5f145c1
Mingzhou666 <mingzhou.fu@gmail.com>
2018-09-23 17:20:09 -0400

    Joy's commit1

--- gofish.py
+++ gofish.py
@@ -1,5 +1,5 @@
 import random
-###
+
 class Card(object):
     suit_names = ["Diamonds", "Clubs", "Hearts", "Spades"]
     rank_levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -16,7 +16,6 @@ class Card(object):
     def __str__(self):
         return "{} of {}".format(self.rank, self.suit)
 
-
 class Deck(object):
     def __init__(self):  # Don't need any input to create a deck of cards
         # This working depends on Card class existing above
@@ -26,20 +25,20 @@ class Deck(object):
                 card = Card(suit, rank)
                 self.cards.append(card)  # appends in a sorted order
 
-def __str__(self):
-    total = []
-    for card in self.cards:
-        total.append(card.__str__())
-        # shows up in whatever order the cards are in
-        return "\n".join(total)  # returns a multi-line string listing each card
-    
+    def __str__(self):
+        total = []
+        for card in self.cards:
+            total.append(card.__str__())
+            # shows up in whatever order the cards are in
+            return "\n".join(total)  # returns a multi-line string listing each card
+        
     def pop_card(self, i=-1):
         # removes and returns a card from the Deck
         # default is the last card in the Deck
         return self.cards.pop(i)  # this card is no longer in the deck -- taken off
 
-def shuffle(self):
-    random.shuffle(self.cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
     
     def replace_card(self, card):
         card_strs = []  # forming an empty list
@@ -48,49 +47,36 @@ def shuffle(self):
         if card.__str__() not in card_strs:  # if the string representing this card is not in the list already
             self.cards.append(card)  # append it to the list
 
-def sort_cards(self):
-    # Basically, remake the deck in a sorted way
-    # This is assuming you cannot have more than the normal 52 cars in a deck
-    self.cards = []
-    for suit in range(4):
-        for rank in range(1, 14):
-            card = Card(suit, rank)
-            self.cards.append(card)
+    def sort_cards(self):
+        # Basically, remake the deck in a sorted way
+        # This is assuming you cannot have more than the normal 52 cars in a deck
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
 
-####### Extra credit for this part starts here #######
 
-# Add a function “deal” to Deck that takes two parameters representing the number of hands and the number
-# of cards per hand and returns a list of Hands. If the number of cards per hand is set to -1, all of the
-# cards should be dealt, even if this results in an uneven number of cards per hand. Write tests to verify
-# that this works.
-def deal(self, num_hand, card_per_hand):
-    
-    round = 0
-        hands = []
+    # Add a function “deal” to Deck that takes two parameters representing the number of hands and the number
+    # of cards per hand and returns a list of Hands. If the number of cards per hand is set to -1, all of the
+    # cards should be dealt, even if this results in an uneven number of cards per hand. Write tests to verify
+    # that this works.
+    def deal(self, num_hand, card_per_hand):
         
-        for i in range(0, num_hand):
-            hands.append(Hand([]))
-        
-        while (card_per_hand == -1 or round < card_per_hand) and len(self.cards):
+        round = 0
+            hands = []
+            
             for i in range(0, num_hand):
-                if len(self.cards):
-                    card = self.pop_card()
-                    hands[i].add_card(card)
-            round += 1
-        
-        return hands
-
-
-####### Extra credit ends here #######
-
-
-######### DO NOT CHANGE CODE ABOVE THIS LINE #########
-
-## You can write any additional debugging/trying stuff out code here...
-## OK to add debugging print statements, but do NOT change functionality of existing code.
-## Also OK to add comments!
-
-#########
+                hands.append(Hand([]))
+            
+            while (card_per_hand == -1 or round < card_per_hand) and len(self.cards):
+                for i in range(0, num_hand):
+                    if len(self.cards):
+                        card = self.pop_card()
+                        hands[i].add_card(card)
+                round += 1
+            
+            return hands
 
 class Hand:
     # create the Hand with an initial set of cards
@@ -116,16 +102,16 @@ class Hand:
         if card.__str__() not in card_strs:
             self.cards.append(card)
 
-# remove a card from the hand
-# param: the card to remove
-# returns: the card, or None if the card was not in the Hand
-def remove_card(self, card):
-    card_str = card.__str__()
-    for existing_card in self.cards:
-        if existing_card.__str__() == card_str:
-            self.cards.remove(existing_card)
-            return existing_card
-        return None
+    # remove a card from the hand
+    # param: the card to remove
+    # returns: the card, or None if the card was not in the Hand
+    def remove_card(self, card):
+        card_str = card.__str__()
+        for existing_card in self.cards:
+            if existing_card.__str__() == card_str:
+                self.cards.remove(existing_card)
+                return existing_card
+            return None
     
     # draw a card from a deck and add it to the hand
     # side effect: the deck will be depleted by one card
@@ -155,6 +141,9 @@ def remove_card(self, card):
         for card in cards_to_remove:
             self.cards.remove(card)
 
+###############################
+# Extra credit 1 starts here
+###############################
 
 def play_go_fish_human():
     play_go_fish(2)
@@ -170,8 +159,8 @@ def play_go_fish(player_count):
     hands = deck.deal(player_count, initial_card_per_player)
     faces = []
 
-for i in range(player_count):
-    faces.append([])
+    for i in range(player_count):
+        faces.append([])
     
     current_player_index = 0
     
@@ -203,10 +192,9 @@ for i in range(player_count):
                 current_player_index = next_player_index
                 continue
 
-asked_rank = 0
-    
-    
-    prompt = current_player_name + ": Please choose a card rank you would like to ask the other player if they have (between 1-13):"
+        asked_rank = 0
+        
+        prompt = current_player_name + ": Please choose a card rank you would like to ask the other player if they have (between 1-13):"
         asked_rank = ask_for_rank(prompt, current_player)
         
         
@@ -219,6 +207,7 @@ asked_rank = 0
                 next_player.remove_card(card)
                 current_player.add_card(card)
                 print(next_player_name + ": Give you " + str(card))
+                
                 dict = {}
                 current_player_cards = current_player.cards.copy()
                 for card in current_player_cards:
@@ -232,25 +221,25 @@ asked_rank = 0
                     if count == 4:
                         new_faces.append(face)
     
-        for card in current_player_cards:
-            if card.rank in new_faces:
-                current_player.remove_card(card)
-                
+                for card in current_player_cards:
+                    if card.rank in new_faces:
+                        current_player.remove_card(card)
+                        
                 for face in new_faces:
                     faces[current_player_index].append(face)
 
-deck_has_card = len(deck.cards) != 0
-    if should_go_fish and deck_has_card:
-        print(next_player_name + " Go fish.")
-        drawed_card = current_player.draw(deck)
-        if drawed_card.rank != asked_rank:
-            dict = {}
-                current_player_cards = current_player.cards.copy()
-                for card in current_player_cards:
-                    if card.rank in dict:
-                        dict[card.rank] += 1
-                    else:
-                        dict[card.rank] = 1
+        deck_has_card = len(deck.cards) != 0
+            if should_go_fish and deck_has_card:
+                print(next_player_name + " Go fish.")
+                drawed_card = current_player.draw(deck)
+                if drawed_card.rank != asked_rank:
+                    dict = {}
+                        current_player_cards = current_player.cards.copy()
+                        for card in current_player_cards:
+                            if card.rank in dict:
+                                dict[card.rank] += 1
+                            else:
+                                dict[card.rank] = 1
         
             new_faces = []
                 for face, count in dict.items():
